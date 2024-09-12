57b9211f1abe968286c53b1fe56c87aa9bd71b4f
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2024-09-12 17:40:14 +0100

    just commit - geomart backend for users and admin [backend with express]

--- payments/paystackpay.js
+++ payments/paystackpay.js
@@ -1,76 +1,76 @@
-// const https = require("https");
-// require("dotenv").config();
+const https = require("https");
+require("dotenv").config();
 
-// const payWithPaystack = (email, amount) => {
-//   return new Promise((resolve, reject) => {
-//     const params = JSON.stringify({
-//       email,
-//       amount,
-//     });
+const payWithPaystack = (email, amount) => {
+  return new Promise((resolve, reject) => {
+    const params = JSON.stringify({
+      email,
+      amount,
+    });
 
-//     const options = {
-//       hostname: "api.paystack.co",
-//       port: 443,
-//       path: "/transaction/initialize",
-//       method: "POST",
-//       headers: {
-//         Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
-//         "Content-Type": "application/json",
-//       },
-//     };
+    const options = {
+      hostname: "api.paystack.co",
+      port: 443,
+      path: "/transaction/initialize",
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+        "Content-Type": "application/json",
+      },
+    };
 
-//     const req = https
-//       .request(options, (res) => {
-//         let data = "";
+    const req = https
+      .request(options, (res) => {
+        let data = "";
 
-//         res.on("data", (chunk) => {
-//           data += chunk;
-//         });
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
 
-//         res.on("end", () => {
-//           resolve(JSON.parse(data));
-//         });
-//       })
-//       .on("error", (error) => {
-//         console.error(error);
-//         reject({ status: false, message: error.message });
-//       });
+        res.on("end", () => {
+          resolve(JSON.parse(data));
+        });
+      })
+      .on("error", (error) => {
+        console.error(error);
+        reject({ status: false, message: error.message });
+      });
 
-//     req.write(params);
-//     req.end();
-//   });
-// };
+    req.write(params);
+    req.end();
+  });
+};
 
-// const paymentStatus = (refrence) => {
-//   return new Promise((resolve, reject) => {
-//     const options = {
-//       hostname: "api.paystack.co",
-//       port: 443,
-//       path: `/transaction/verify/${refrence}`,
-//       method: "GET",
-//       headers: {
-//         Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
-//       },
-//     };
+const paymentStatus = (refrence) => {
+  return new Promise((resolve, reject) => {
+    const options = {
+      hostname: "api.paystack.co",
+      port: 443,
+      path: `/transaction/verify/${refrence}`,
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+      },
+    };
 
-//     https
-//       .request(options, (res) => {
-//         let data = "";
+    https
+      .request(options, (res) => {
+        let data = "";
 
-//         res.on("data", (chunk) => {
-//           data += chunk;
-//         });
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
 
-//         res.on("end", () => {
-//           console.log(JSON.parse(data));
-//           resolve(JSON.parse(data));
-//         });
-//       })
-//       .on("error", (error) => {
-//         console.error(error);
-//         reject({ status: false, message: error.message });
-//       });
-//   });
-// };
+        res.on("end", () => {
+          console.log(JSON.parse(data));
+          resolve(JSON.parse(data));
+        });
+      })
+      .on("error", (error) => {
+        console.error(error);
+        reject({ status: false, message: error.message });
+      });
+  });
+};
 
-// module.exports = { payWithPaystack, paymentStatus };
+module.exports = { payWithPaystack, paymentStatus };
