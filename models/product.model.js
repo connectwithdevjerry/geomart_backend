d2666484074d8a0b735ef281ed387537726f5334
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2024-09-15 17:38:11 +0100

    just commit - geomart backend for users and admin [backend with express]

--- models/product.model.js
+++ models/product.model.js
@@ -1,22 +1,22 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const ProductSchema = mongoose.Schema({
-//   productName: { type: String, required: true },
-//   productPrice: { type: Number, required: true },
-//   description: { type: String, required: true },
-//   productImage: { type: String, required: true },
-//   dateAdded: { type: String, default: Date.now() },
-//   productSku: { type: String, required: true },
-//   quantity: { type: Number, default: 0 },
-//   productCategory: { type: String, required: true },
-//   sales: { type: Number, default: 0 },
-//   bestSelling: { type: Boolean, default: false },
-//   featured: { type: Boolean, default: false },
-//   category: {
-//     type: String,
-//     enum: ["physical", "digital", "electronics", "fashion", "home_and_garden"],
-//   },
-// });
+const ProductSchema = mongoose.Schema({
+  productName: { type: String, required: true },
+  productPrice: { type: Number, required: true },
+  description: { type: String, required: true },
+  productImage: { type: String, required: true },
+  dateAdded: { type: String, default: Date.now() },
+  productSku: { type: String, required: true },
+  quantity: { type: Number, default: 0 },
+  productCategory: { type: String, required: true },
+  sales: { type: Number, default: 0 },
+  bestSelling: { type: Boolean, default: false },
+  featured: { type: Boolean, default: false },
+  category: {
+    type: String,
+    enum: ["physical", "digital", "electronics", "fashion", "home_and_garden"],
+  },
+});
 
-// const ProductModel = mongoose.model("products_collection", ProductSchema);
-// module.exports = ProductModel;
+const ProductModel = mongoose.model("products_collection", ProductSchema);
+module.exports = ProductModel;
