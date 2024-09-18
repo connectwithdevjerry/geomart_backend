f457305c39aa9a0f4a182f56579d365c4ed0b198
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2024-09-18 17:35:41 +0100

    just commit - geomart backend for users and admin [backend with express]

--- controller/ticket.controller.js
+++ controller/ticket.controller.js
@@ -1,27 +1,27 @@
-// const ticketModel = require("../models/ticket.models");
-// const { ticketSchema } = require("../validation_schema");
+const ticketModel = require("../models/ticket.models");
+const { ticketSchema } = require("../validation_schema");
 
-// const createTicket = async (req, res) => {
-//   try {
-//     const ticketDetails = await ticketSchema.validateAsync(req.body);
+const createTicket = async (req, res) => {
+  try {
+    const ticketDetails = await ticketSchema.validateAsync(req.body);
     
-//     const ticket = new ticketModel({
-//       ...ticketDetails,
-//       status: "pending",
-//     });
+    const ticket = new ticketModel({
+      ...ticketDetails,
+      status: "pending",
+    });
 
-//     ticket.save();
+    ticket.save();
 
-//     res.send({ status: true, message: "Ticket created successfully" });
-//   } catch (error) {
-//     if (error.isJoi === true) {
-//       error.status = 422;
-//     }
+    res.send({ status: true, message: "Ticket created successfully" });
+  } catch (error) {
+    if (error.isJoi === true) {
+      error.status = 422;
+    }
 
-//     return res.send({ status: false, message: "Ticket could not be created!" });
-//   }
-// };
+    return res.send({ status: false, message: "Ticket could not be created!" });
+  }
+};
 
-// module.exports = {
-//   createTicket,
-// };
+module.exports = {
+  createTicket,
+};
