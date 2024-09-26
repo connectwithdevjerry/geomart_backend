e91b401f81d9bbf6ed6c21d3d153274fb84840c8
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2024-09-26 17:42:51 +0100

    just commit - geomart backend for users and admin [backend with express]

--- routes/products.route.js
+++ routes/products.route.js
@@ -1,27 +1,27 @@
-// const express = require("express");
-// const router = express.Router();
-// const {
-//   createProduct,
-//   updateProduct,
-//   getProduct,
-//   deleteProduct,
-//   createProducts,
-//   updateProducts,
-//   getProducts,
-//   deleteProducts,
-//   searchProduct,
-// } = require("../controller/product.controller");
+const express = require("express");
+const router = express.Router();
+const {
+  createProduct,
+  updateProduct,
+  getProduct,
+  deleteProduct,
+  createProducts,
+  updateProducts,
+  getProducts,
+  deleteProducts,
+  searchProduct,
+} = require("../controller/product.controller");
 
-// router.post("/create_product", createProduct);
-// router.put("/update_product/:id", updateProduct);
-// router.get("/get_product/:id", getProduct);
-// router.delete("/delete_product/:id", deleteProduct);
+router.post("/create_product", createProduct);
+router.put("/update_product/:id", updateProduct);
+router.get("/get_product/:id", getProduct);
+router.delete("/delete_product/:id", deleteProduct);
 
-// router.get("/search_products", searchProduct);
+router.get("/search_products", searchProduct);
 
-// router.get("/get_products", getProducts);
-// router.post("/create_products", createProducts);
-// router.put("/update_products", updateProducts);
-// router.delete("/delete_products", deleteProducts);
+router.get("/get_products", getProducts);
+router.post("/create_products", createProducts);
+router.put("/update_products", updateProducts);
+router.delete("/delete_products", deleteProducts);
 
-// module.exports = router;
+module.exports = router;
